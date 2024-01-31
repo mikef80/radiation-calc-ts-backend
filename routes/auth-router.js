87ced8e5ab5926a6971c65264d9c48225a2b1514
Mike <mike@mike-francis.org>
2024-01-31 11:01:22 +0000

    parking here

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,8 +1,11 @@
+const nodemailer = require("nodemailer");
+
 const { signup, login } = require("../api/controllers/auth-controller");
 
 const authRouter = require("express").Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
+
 
 module.exports = authRouter;
